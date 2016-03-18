0e3d443f442eeb0a40c22202e76704c14fd5298e
Jason Wu <jason@jasondembp.pft.com>
2016-03-18 17:48:49 +0800

    first commit

---
+++ MinnieJasonWedding/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MinnieJasonWedding
+//
+//  Created by Jason Wu on 2016/3/18.
+//  Copyright © 2016年 Jason Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MinnieJasonWedding/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MinnieJasonWedding
+//
+//  Created by Jason Wu on 2016/3/18.
+//  Copyright © 2016年 Jason Wu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
